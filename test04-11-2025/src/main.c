7fb80e9ecd7381567659d5a2e9967d9af60761d5
drus56 <zxandruxazx56@yandex.ru>
2025-11-16 23:50:26 +0500

    ch

--- test04-11-2025/src/main.c
+++ test04-11-2025/src/main.c
@@ -3,12 +3,38 @@
 #include <stdbool.h>
 
 int main(int argc, char* argv[]) {
-    if (!SDL_HasGamepad()) {
-        printf("No gamepads connected\n");
-        SDL_Quit();
-        return 0;
-    }
+        SDL_Init(SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC );
+
+    // if (!SDL_HasGamepad()) {
+    //     printf("No gamepads connected\n");
+    //     SDL_Quit();
+    //     return 0;
+    // }
     printf("Gamepad found!\n");
     // Init window and renderer
-    SDL_Init(SDL_INIT_VIDEO);
+
+    int count = 0;
+SDL_JoystickID *ids = SDL_GetGamepads(&count);
+SDL_Gamepad* gamepad = NULL;
+
+// Iterate over the list of gamepads
+for(int i = 0; i < count; i++) {
+    SDL_Gamepad* gamepd = SDL_OpenGamepad(ids[i]);
+    if(gamepad == NULL) {
+        gamepad = gamepd;
+    }
+    
+    printf("Gamepad connected: %s\n", SDL_GetGamepadName(gamepd));
+    
+    // Close the other gamepads
+    if(i > 0) {
+        SDL_CloseGamepad(gamepd);
+    }
+}
+if (!gamepad) {
+    printf("Failed to open gamepad: %s\n", SDL_GetError());
+    SDL_Quit();
+    return 1;
+}
+
 }
